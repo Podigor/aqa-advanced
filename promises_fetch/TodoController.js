aa774ca0484a84ffe307ed9bec7b9405708fcada
Ihor Podielnikov <Podigor@i.ua>
2024-02-20 01:18:38 +0200

    Home task for promises and fetch

---
+++ promises_fetch/TodoController.js
@@ -0,0 +1,10 @@
+export default class TodoController {
+    constructor(baseUrl) {
+        this._baseUrl = baseUrl
+    }
+        async fetchToDoById(id) {
+        const response = await fetch(`${this._baseUrl}/todos/${id}`)
+        const bodyJson = await response.json()
+        return bodyJson
+    }
+}
---
+++ promises_fetch/UserController.js
@@ -0,0 +1,10 @@
+export default class UserController {
+    constructor(baseUrl) {
+        this._baseUrl = baseUrl
+    }
+        async fetchUserById(id) {
+        const response = await fetch(`${this._baseUrl}/users/${id}`)
+        const bodyJson = await response.json()
+        return bodyJson
+    }
+}
